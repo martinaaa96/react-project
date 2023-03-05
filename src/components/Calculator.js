bd7b51de5c51aea053f316977bad299843968036
Martina Vangelova <mvangelovaa@abv.bg>
2023-03-05 16:10:13 +0200

    create  file components

---
+++ src/components/Calculator.js
@@ -0,0 +1,28 @@
+export default function Calculator(){
+
+    return (
+        <div className="calculator">
+        <div className="calculator__output">0</div>
+        <div className="calculator__keys">
+          <button className="calculator__key calculator__key--operator">+</button>
+          <button className="calculator__key calculator__key--operator">-</button>
+          <button className="calculator__key calculator__key--operator">&times;</button>
+          <button className="calculator__key calculator__key--operator">÷</button>
+          <button className="calculator__key">7</button>
+          <button className="calculator__key">8</button>
+          <button className="calculator__key">9</button>
+          <button className="calculator__key">4</button>
+          <button className="calculator__key">5</button>
+          <button className="calculator__key">6</button>
+          <button className="calculator__key">1</button>
+          <button className="calculator__key">2</button>
+          <button className="calculator__key">3</button>
+          <button className="calculator__key">0</button>
+          <button className="calculator__key">.</button>
+          <button className="calculator__key">AC</button>
+          <button className="calculator__key calculator__key--enter">=</button>
+        </div>
+      </div>
+
+    )
+}
