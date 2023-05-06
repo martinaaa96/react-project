923217f9e2bc9c1304103829b4de4ba5cceee9dd
Martina Vangelova <mvangelovaa@abv.bg>
2023-05-06 15:59:53 +0300

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Calculator from "./components/Calculator";
-
+import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1>Welcome to Calculator app</h1>
       <Calculator/>
      
     </div>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,59 @@
-import Buttons from "./Buttons";
-import Operator from "./Operator";
-import Output from "./Output";
 
-import style from '../css/App.module.css'
 
 import { useState } from "react";
 
-
-
-
 export default function Calculator(){
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-  const [result , setResult ] = useState("");
-
-
-  const handleClick = (e)=>{
-    setResult(result.concat(e.target.name))
-  }
-
-  const handleClear = ()=>{
-    setResult("");
-  }
-
-  const handleDelete = ()=>{
-
-    setResult(result.slice(0,-1));
-
-  }
-
-  const handleResult = (e)=>{
-
-    try {
-      setResult(eval(result).toString());
-
-    }catch(err){
-      setResult("Error");
-
+  const handleClick = (event) => {
+    const value = event.target.value;
+    switch (value) {
+      case "AC":
+        setInput("");
+        setResult("");
+        break;
+      case ".":
+        setInput(input.slice(0, -1));
+        break;
+      case "=":
+        try {
+          setResult(eval(input).toString());
+        } catch (error) {
+          setResult("Invalid Calculation");
+        }
+        break;
+      default:
+        setInput(input + value);
+        break;
     }
   }
+ 
     return (
-        <div className={style["calculator"]}>
-        <Output />
-        <div className={style["calculator__keys"]}>
-          <Operator/>
-         <Buttons/>
+        <div className="calculator">
+        <input className="calculator__output" type="text" defaultValue={input}/>
+        <div className="calculator__keys">
+        <button className="calculator__key" onClick={handleClick} value="+">+</button>
+          <button className="calculator__key" onClick={handleClick} value="-">-</button>
+          <button className="calculator__key" onClick={handleClick} value="*">&times;</button>
+          <button className="calculator__key" onClick={handleClick} value="/">÷</button>
+          
+
+         <button className="calculator__key" onClick={handleClick} value="7">7</button>
+        <button className="calculator__key"  onClick={handleClick} value="8">8</button>
+        <button className="calculator__key" onClick={handleClick} value="9">9</button>
+        <button className="calculator__key" onClick={handleClick} value="4">4</button>
+        <button className="calculator__key" onClick={handleClick} value="5">5</button>
+        <button className="calculator__key" onClick={handleClick} value="6">6</button>
+        
+        <button className="calculator__key" onClick={handleClick} value="1">1</button>
+        <button className="calculator__key"onClick={handleClick} value="2">2</button>
+        <button className="calculator__key" onClick={handleClick}value="3">3</button>
+        <button className="calculator__key" onClick={handleClick} value="0">0</button>
+       
+        <button className="calculator__key" onClick={handleClick} value=".">.</button>
+        <button className="calculator__key" onClick={handleClick} value="AC">AC</button>
+        <button className="calculator__key--enter" onClick={handleClick} value="=">=</button>
           
         </div>
       </div>
