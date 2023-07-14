1de5ad331baad9085334802d02e479cc1efa3bec
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-14 15:18:26 +0300

    components Calculator fixed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,39 +2,37 @@
 import { useState } from "react";
 
 export default function Calculator(){
-  const [value, setValue] = useState("");
-  const [result, setResult] = useState("");
+ const [result, setResult] = useState('')
 
   
  
     return (
-        <div className="calculator">
-        <input className="calculator__output" type="text" value={value}/>
-        <div className="calculator__keys">
-        <button className="calculator__key"  value="+">+</button>
-          <button className="calculator__key" value="-">-</button>
-          <button className="calculator__key"  value="*">&times;</button>
-          <button className="calculator__key"  value="/">÷</button>
-          
+        <div className="container">
+<form>
+<input type="text" value={result}/>
+</form>
+<div className="keypad">
+<button>Clear</button>
+<button>C</button>
+<button>&divide;</button>
+<button>7</button>
+<button>8</button>
+<button>9</button>
+<button>&times;</button>
+<button>4</button>
+<button>5</button>
+<button>6</button>
+<button>&ndash;</button>
+<button>1</button>
+<button>2</button>
+<button>3</button>
+<button>+</button>
+<button>0</button>
+<button>.</button>
+<button>=</button>
 
-         <button className="calculator__key"  value="7">7</button>
-        <button className="calculator__key"  value="8">8</button>
-        <button className="calculator__key"  value="9">9</button>
-        <button className="calculator__key"  value="4">4</button>
-        <button className="calculator__key"  value="5">5</button>
-        <button className="calculator__key"  value="6">6</button>
-        
-        <button className="calculator__key"  value="1">1</button>
-        <button className="calculator__key" value="2">2</button>
-        <button className="calculator__key" value="3">3</button>
-        <button className="calculator__key"  value="0">0</button>
-       
-        <button className="calculator__key"  value=".">.</button>
-        <button className="calculator__key"  value="AC">AC</button>
-        <button className="calculator__key--enter" value="=">=</button>
-          
+</div>
         </div>
-      </div>
 
     )
 }
