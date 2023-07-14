e6c87e1420d752e46581776bfbd5681ab7e4a5bd
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-14 19:44:08 +0300

    calculate function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,48 +2,57 @@
 import { clear } from "@testing-library/user-event/dist/clear";
 import { useState } from "react";
 
-export default function Calculator(){
- const [result, setResult] = useState('')
+export default function Calculator() {
+  const [result, setResult] = useState('')
 
-  const handleClick = (e)=>{
-setResult(result.concat(e.target.name));
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
 
 
   }
-  const clear = ()=>{
-setResult("")
+  const clear = () => {
+    setResult("");
   }
-  const backspace = ()=>{
-  
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
   }
- 
-    return (
-        <div className="container">
-<form>
-<input type="text" value={result}/>
-</form>
-<div className="keypad">
-<button onClick={clear} id="clear" >Clear</button>
-<button onClick={backspace} id="backspace">C</button>
-<button name="/" onClick={handleClick}>&divide;</button>
-<button name="7" onClick={handleClick}>7</button>
-<button name="8" onClick={handleClick}>8</button>
-<button name="9" onClick={handleClick}>9</button>
-<button name="*"onClick={handleClick}>&times;</button>
-<button name="4" onClick={handleClick}>4</button>
-<button name="5" onClick={handleClick}>5</button>
-<button name="6" onClick={handleClick}>6</button>
-<button name="-" onClick={handleClick}>&ndash;</button>
-<button name="1" onClick={handleClick}>1</button>
-<button name="2" onClick={handleClick}>2</button>
-<button name="3" onClick={handleClick}>3</button>
-<button name="+" onClick={handleClick}>+</button>
-<button name="0" onClick={handleClick}>0</button>
-<button name="." onClick={handleClick}>.</button>
-<button onClick={handleClick} id="result">=</button>
+  const calculate = () => {
 
-</div>
-        </div>
+    try {
+      setResult(eval(result).toString())
+    } catch (err) {
+      setResult("Error");
+    }
 
-    )
+  }
+
+  return (
+    <div className="container">
+      <form>
+        <input type="text" defaultValue={result} />
+      </form>
+      <div className="keypad">
+        <button className="hightlight" onClick={clear} id="clear" >Clear</button>
+        <button className="hightlight" onClick={backspace} id="backspace">C</button>
+        <button className="hightlight" name="/" onClick={handleClick}>&divide;</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button className="hightlight" name="*" onClick={handleClick}>&times;</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button className="hightlight" name="-" onClick={handleClick}>&ndash;</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button className="hightlight" name="+" onClick={handleClick}>+</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button className="hightlight" onClick={calculate} id="result">=</button>
+
+      </div>
+    </div>
+
+  )
 }
